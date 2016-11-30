31c8d5c53cd22a770c6f416698b790ad6ccbc12e
RoyalMjz <Royal7Mjz@gmail.com>
2016-11-30 22:25:16 +0800

    update

---
+++ JZPictureKeyboardDemo/JZPictureKeyboardDemo/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  JZPictureKeyboard
+//
+//  Created by RoyalMjz on 15/11/28.
+//  Copyright © 2015年 RoyalMjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ JZPictureKeyboardDemo/JZPictureKeyboardDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JZPictureKeyboardDemo
+//
+//  Created by RoyalMjz on 16/11/30.
+//  Copyright © 2016年 RoyalMjz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
